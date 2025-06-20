9ba80fe319ff0900765792d0f4ae5e0d492e5272
saritakarwaa <karwasarita672@gmail.com>
2025-06-20 21:12:47 +0530

    initial setup

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
