ce5897f40ab7308641bef412cbd3180555481f07
saritakarwaa <karwasarita672@gmail.com>
2025-06-21 22:12:46 +0530

    add cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,19 @@ import dotenv from 'dotenv'
 
 const app = express();
 dotenv.config()
-app.use(cors());
+
+const allowedOrigins = ["http://localhost:5173", ];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
